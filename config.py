0b5be42fc4c3a67f79f6063aec32f421157f2782
Arjun <99074495+Wuhuiuusushsg@users.noreply.github.com>
2025-03-28 14:49:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,4 +18,4 @@ DB_URI = os.environ.get("DB_URI", "mongodb+srv://motivationhubiit2025:75RgOXQzxY
 DB_NAME = os.environ.get("DB_NAME", "ARJUN")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', Flase))
